686ec8879005c282f3fd098442d02afd2da95612
Isaac Alexander <Isaacalexander81@gmail.com>
2025-08-29 02:07:02 +0100

    src/users/users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) { }
+
+    //get all users
+    async findall(): Promise<User[]> {
+        return await this.usersRepository.find();
+    }
+
+    // get one user
+    async findOne(id: number): Promise<User | null> {
+        return await this.usersRepository.findOne({ where: { id } });
+    }
+
+    // create user
+    async create(user: User): Promise<User> {
+        const newUser = this.usersRepository.create(user);
+        return await this.usersRepository.save(newUser);
+    }
+
+    // Update user
+    async update(id: number, user: User): Promise<User | null> {
+        await this.usersRepository.update(id, user);
+        return await this.usersRepository.findOne({ where: { id } });
+    }
+
+    // delete user
+    async delete(id: number): Promise<void> {
+        await this.usersRepository.delete(id);
+        
+    }
+}
